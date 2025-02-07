201ea2600c192173a6ea5f8cc73a1d959855fe79
josevieira2205 <2242256@my.ipleiria.pt>
2025-02-07 22:26:21 +0000

    added LV led function
    read voltage whit DMA and timer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -49,15 +49,18 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+uint16_t adc_counter = 0;
+uint8_t data_trigger = 0;
+uint16_t lv_counter = 0;
+
 uint16_t heartbeat_counter = 0;
 uint8_t heartbeat_state = 0;
 
 
 uint16_t current = 0; 
-uint16_t voltage = 0;
+uint16_t raw_voltage = 0;
 uint16_t rawValeus [2];
-uint16_t voltage_int; 
-uint16_t voltage_frac;
+
 
 /* USER CODE END PV */
 
@@ -65,9 +68,12 @@ uint16_t voltage_frac;
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 
-void PWMDutyCycleSet(uint16_t ChannelName, uint32_t dutyCycle); // Function prototype for setting the duty cycle of the PWM signal
-void SendData(uint16_t raw_voltage); // Function prototype for sending data over CAN bus
+
 void heartbeat(void); // Function prototype for sending the heartbeat trigger
+void StartADC1(void); // Function prototype for starting the ADC
+void VoltageMessure(uint16_t adc_valeu); // Function prototype for sending data over CAN bus
+
+void PWMDutyCycleSet(uint16_t ChannelName, uint32_t dutyCycle); // Function prototype for setting the duty cycle of the PWM signal
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -80,16 +86,23 @@ void TIM3_IRQHandler(void)
   HAL_TIM_IRQHandler(&htim3);
   /* USER CODE BEGIN TIM3_IRQn 1 */
     heartbeat_counter++; // Increment the heartbeat counter
+    adc_counter++; // Increment the ADC counter
+    data_trigger ++; // Set the data trigger
+    lv_counter ++; // Increment the LV counter
+
   /* USER CODE END TIM3_IRQn 1 */
 }
 
-uint8_t convCompleted = 0; // Variable to check if the conversion is completed
+
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
+  for (uint8_t i = 0; i < hadc1.Init.NbrOfConversion; i++){
+    current = rawValeus[0]; // Current value
+    raw_voltage = rawValeus[1]; // Voltage value
+}
 
-uint8_t convCompleted = 1; // Set the conversion completed flag to 1
-//HAL_ADC_Start_IT(&hadc1); // Start the ADC in interrupt mode
+
 }
 /* USER CODE END 0 */
 
@@ -148,17 +161,13 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-      while (!convCompleted)
-      {
-        for (uint8_t i = 0; i < hadc1.Init.NbrOfConversion; i++){
-            current = rawValeus[0]; // Current value
-            voltage = rawValeus[1]; // Voltage value
-        }
 
-        SendData(voltage); // Send the voltage value over CAN bus 
+
+        StartADC1(); // Start the ADC
+        VoltageMessure(raw_voltage); // Send the voltage value over CAN bus 
         heartbeat(); // Send the heartbeat trigger
-      }
-      
+
+
 
   }
   /* USER CODE END 3 */
@@ -211,39 +220,92 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-void heartbeat(void)
-{
 
-  if (heartbeat_state<= 2 && heartbeat_counter >= 1){
-    
-      HAL_GPIO_TogglePin(GPIOC, Led_Debug_3_Pin); // Toggle the LED
+
+void heartbeat(void){
+
+  if (heartbeat_state<= 2 && heartbeat_counter >= 2){ // 200 ms
+
+      HAL_GPIO_TogglePin(GPIOC, Led_Debug_1_Pin); // Toggle the LED
       heartbeat_state ++;
       heartbeat_counter = 0;
     
   } else{
-    if (heartbeat_counter >= 7){ // 700 ms
+    if (heartbeat_counter >= 9){ // 900 ms
 
-      HAL_GPIO_TogglePin(GPIOC, Led_Debug_3_Pin); // Toggle the LED
+      HAL_GPIO_TogglePin(GPIOC, Led_Debug_1_Pin); // Toggle the LED
       heartbeat_state = 0;
       heartbeat_counter = 0;
     
-      }
-    
+      }    
     }
-  
 }
 
 
 
-void SendData(uint16_t raw_voltage)
+
+void StartADC1(void)
 {
+  if (adc_counter >= 1){ // 100 ms
+    adc_counter = 0;
+    HAL_ADC_Start_IT(&hadc1); // Start the ADC in interrupt mode
+  }
+}
+
+void VoltageMessure(uint16_t raw_voltage){
+
+    static uint16_t voltage_int = 0; 
+    static uint16_t voltage_frac = 0;
+    static uint16_t voltage_mv = 0;
+    static uint8_t lv_state = 0;
     char msg[100]; // Message buffer
 
     voltage_int = raw_voltage * 28.5 / 4095; // Voltage value
     voltage_frac = (raw_voltage * 28500 / 4095) % 1000; // Voltage value
+    voltage_mv = raw_voltage * 28500 / 4095; // Voltage value
 
-    sprintf(msg, "Voltage: %d.%03d\n ",voltage_int,voltage_frac); // Print the current and voltage values
-    HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY); // Transmit the message to the UART
+    if (voltage_mv >= 20000){ // 28.5 V
+      lv_state = 2;
+    } else if (voltage_mv >= 10000){ // 27 V
+      lv_state = 1;
+    } else {
+      lv_state = 0;
+    }
+
+    switch (lv_state){
+    case 2:
+        lv_counter = 0;
+        HAL_GPIO_WritePin(GPIOC, Led_Debug_3_Pin, GPIO_PIN_SET); // Turn on the LED
+        break;
+      
+    case 1:
+      if (lv_counter >= 7){ // 500 ms
+        lv_counter = 0;
+        HAL_GPIO_TogglePin(GPIOC, Led_Debug_3_Pin); // Turn on the LED
+      }
+      break;
+
+    case 0:
+      if (lv_counter >= 2){ // 100 ms
+        lv_counter = 0;
+        HAL_GPIO_TogglePin(GPIOC, Led_Debug_3_Pin); // Turn off the LED
+      } 
+      break;
+
+    default:
+      lv_counter = 0;
+      HAL_GPIO_WritePin(GPIOC, Led_Debug_3_Pin, GPIO_PIN_RESET); // Turn on the LED
+      break;
+    }
+
+
+    if (data_trigger >= 10){ // 1000 ms
+      data_trigger = 0;
+      sprintf(msg, "Voltage: %d.%03d\n ",voltage_int,voltage_frac); // Print the current and voltage values
+      HAL_UART_Transmit(&huart1, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY); // Transmit the message to the UART
+    }
+
+
 }
 
 // Function to set the duty cycle of the PWM signal 
